a56fb1b9f8daf0f7a808aa5d417dd1e2eaeefeb4
yeseul0809 <dresulsul@naver.com>
2024-06-18 02:59:36 +0900

    [Todo 1 : solve]

--- src/lib/utils/validateEmail.js
+++ src/lib/utils/validateEmail.js
@@ -1,2 +1,12 @@
-// TODO 이메일을 입력받아, 해당 이메일이 올바른 이메일 형식인지 확인합니다. 올바르다면 true 올바르지 않다면 false 를 리턴해주세요.
-export const validateEmail = (email) => {};
+// TODO 이메일을 입력받아, 해당 이메일이 올바른 이메일 형식인지 확인합니다.
+// 올바르다면 true 올바르지 않다면 false 를 리턴해주세요.
+
+export const validateEmail = (email) => {
+  const emailReg = /^([0-9a-zA-Z_.-]+)@([0-9a-zA-Z_-]+)(.[0-9a-zA-Z_-]+){1,2}$/;
+
+  if (!emailReg.test(email)) {
+    return false;
+  } else {
+    return true;
+  }
+};
