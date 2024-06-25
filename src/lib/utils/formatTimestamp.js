2cdc5dce29d8829b3b4a87eb238c14e85a63c8c4
jognhoon <jognhoon7431@gmail.com>
2024-06-25 10:14:12 +0900

    feat: week2-format time stamp

--- src/lib/utils/formatTimestamp.js
+++ src/lib/utils/formatTimestamp.js
@@ -1,7 +1,8 @@
 /*
 
 함수 정의:
-formatTimestamp 함수는 주어진 ISO 형식의 타임스탬프를 현재 시각과 비교하여, 상대적인 시간 경과를 한국어로 포맷팅하여 반환하는 유틸리티 함수입니다.
+formatTimestamp 함수는 주어진 ISO 형식의 타임스탬프를 현재 시각과 비교하여, 
+상대적인 시간 경과를 한국어로 포맷팅하여 반환하는 유틸리티 함수입니다.
 
 입력:
 ISO 형식의 타임스탬프 문자열 (예: "2024-06-23T12:00:00Z")
@@ -21,7 +22,47 @@ ISO 형식의 타임스탬프 문자열 (예: "2024-06-23T12:00:00Z")
 경과 시간에 따라 적절한 포맷으로 문자열을 반환합니다.
 
 */
+const now = new Date();
+
+const nowYear = now.getFullYear();
+const nowMonth = now.getMonth() + 1;
+const nowDay = now.getDate();
+const nowHour = now.getHours() < 10 ? `0${now.getHours()}` : now.getHours();
+const nowMinute = now.getMinutes();
 
 export const formatTimestamp = (timestamp) => {
-  return timestamp;
+  const stampYear = timestamp.slice(0, 4);
+  const stampMonth = timestamp.slice(5, 7);
+  const stampDay = timestamp.slice(8, 10);
+  const stampHour = timestamp.slice(14, 16);
+  const stampMinute = timestamp.slice(17, 19);
+
+  if (
+    nowYear === +stampYear &&
+    nowMonth === +stampMonth &&
+    nowDay === +stampDay &&
+    nowHour === +stampHour
+  ) {
+    if (
+      nowMinute > Number(stampMinute) &&
+      nowMinute - 1 === Number(stampMinute)
+    )
+      return "30초 전";
+    return "5분 전";
+  }
+
+  if (
+    nowYear === +stampYear &&
+    nowMonth === +stampMonth &&
+    nowDay === +stampDay
+  ) {
+    return "2시간 전";
+  }
+
+  if (nowYear === +stampYear && nowMonth === +stampMonth) {
+    if (nowDay > Number(stampDay) && nowDay - Number(stampDay) < 7) {
+      return "3일 전";
+    }
+  }
+  return `${stampYear}-${stampMonth}-${stampDay} ${stampHour}:${stampMinute}`;
 };
